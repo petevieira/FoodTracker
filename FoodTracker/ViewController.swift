176da0352aa353997fd9cabadb2209d3bd4dac21
Peter Vieira <peter.vieira@jpl.nasa.gov>
2016-10-26 07:47:14 -0700

    working label and default photo

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
-  override func viewDidLoad() {
+  // MARK: Properties
+  @IBOutlet weak var nameTextField: UITextField!
+  @IBOutlet weak var mealNameLabel: UILabel!
+
+    override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    // set the text field's delegate to be this class
+    nameTextField.delegate = self
+  }
+  // MARK: UITextFieldDelegate
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    // Hide the keyboard
+    textField.resignFirstResponder()
+    return true
+  }
+  
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    mealNameLabel.text = textField.text
+  }
+  
+  // MARK: Actions
+  @IBAction func setDefaultLabelText(_ sender: UIButton) {
+    mealNameLabel.text = "Default Text"
   }
 
 }
