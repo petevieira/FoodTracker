0cc49b1c7ac6cced1ed1dcf7da6ea79355962ea3
Peter Vieira <peter.vieira@jpl.nasa.gov>
2016-10-26 07:57:27 -0700

    added tap gesture recognizer and fixed layout issues with stack view

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
   // MARK: Properties
   @IBOutlet weak var nameTextField: UITextField!
   @IBOutlet weak var mealNameLabel: UILabel!
+  @IBOutlet weak var photoImageView: UIImageView!
 
     override func viewDidLoad() {
     super.viewDidLoad()
@@ -31,6 +32,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
   }
   
   // MARK: Actions
+  @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+  }
+
   @IBAction func setDefaultLabelText(_ sender: UIButton) {
     mealNameLabel.text = "Default Text"
   }
