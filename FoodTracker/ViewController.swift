8007ef566c280d7cc965e8a946923e26129e43ca
Peter Vieira <peter.vieira@jpl.nasa.gov>
2016-10-27 01:12:44 -0700

    able to select an image in the photo library

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate,
+  UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
   // MARK: Properties
   @IBOutlet weak var nameTextField: UITextField!
@@ -31,8 +32,38 @@ class ViewController: UIViewController, UITextFieldDelegate {
     mealNameLabel.text = textField.text
   }
   
+  // MARK: UIImagePickerControllerDelegate
+  func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    // Dismiss the picker if the user canceled.
+    dismiss(animated: true, completion: nil)
+  }
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    // The info dictionary contains multiple representations of the image, and this uses the original.
+    let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+
+    // Set photoImageView to display the selected image.
+    photoImageView.image = selectedImage
+
+    // Dismiss the picker.
+    dismiss(animated: true, completion: nil)
+  }
+  
   // MARK: Actions
   @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+    // hide the keyboard
+    nameTextField.resignFirstResponder()
+
+    // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+    let imagePickerController = UIImagePickerController()
+    
+    // Only allow photos to be picked, not taken.
+    imagePickerController.sourceType = .photoLibrary
+
+    present(imagePickerController, animated: true, completion: nil)
+
+    // Make sure ViewController is notified when the user picks an image.
+    imagePickerController.delegate = self
   }
 
   @IBAction func setDefaultLabelText(_ sender: UIButton) {
