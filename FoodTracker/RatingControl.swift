89750d5ffd7f7556dbc07df08e74fb32fb5ebf3d
Peter Vieira <peter.vieira@jpl.nasa.gov>
2016-10-29 14:33:29 -0700

    added ratingcontrol view subclass. display one button that will eventually be a star

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,34 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Peter Vieira on 10/29/16.
+//  Copyright © 2016 rapierevite. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+  // MARK: Initialization
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+
+    let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+    button.backgroundColor = UIColor.red
+    addSubview(button)
+  }
+
+  override public var intrinsicContentSize: CGSize {
+    return CGSize(width: 240, height: 44)
+  }
+  
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
