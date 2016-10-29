bb3af4d04a47fc526b66637322caf70bea45178a
Peter Vieira <peter.vieira@jpl.nasa.gov>
2016-10-29 14:55:19 -0700

    layed out buttons

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -10,15 +10,31 @@ import UIKit
 
 class RatingControl: UIView {
 
+  // MARK: Properties
+  var rating = 0
+  var ratingButtons = [UIButton]()
+  
   // MARK: Initialization
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
 
-    let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-    button.backgroundColor = UIColor.red
-    button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)),
-                     for: .touchDown)
-    addSubview(button)
+    for _ in 0..<5 {
+      let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+      button.backgroundColor = UIColor.red
+      button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)),
+                       for: .touchDown)
+      ratingButtons += [button]
+      addSubview(button)
+    }
+  }
+  
+  override func layoutSubviews() {
+    var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+    // offset each buttons origin by the width plus spacing
+    for (index, button) in ratingButtons.enumerated() {
+      buttonFrame.origin.x = CGFloat(index * (44 + 5))
+      button.frame = buttonFrame
+    }
   }
 
   override public var intrinsicContentSize: CGSize {
